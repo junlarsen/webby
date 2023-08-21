20f75f150b9a2fe0d8709f37518629d7144b5d84
Mats Larsen <mats@jun.codes>
2023-08-21 17:06:45 +0200

    fix: expand on wording

--- apps/wk/src/app/page.tsx
+++ apps/wk/src/app/page.tsx
@@ -19,6 +19,14 @@ export default function Home() {
           for both the meaning and the reading at once, halving the amount of cards needed to review.
         </p>
 
+        <p>The website is mobile-friendly, making it easy to do your reviews quickly on mobile too.</p>
+
+        <p>
+          This way, the user does not have to fully type out each meaning and answer, significantly reducing the time
+          required to perform your reviews. It is thus more comparable to Anki than the WaniKani review app. If you are
+          concerned about not learning how to use the Japanese input method editor, then simply do not use this website.
+        </p>
+
         <div>
           <h2 className="font-semibold">How it works:</h2>
 
@@ -27,6 +35,22 @@ export default function Home() {
             <li>Press the middle of the screen to reveal the answer</li>
             <li>Press either "correct" or "incorrect" to mark the answer as correct or not</li>
             <li>If you are on desktop, press "F" to exit.</li>
+          </ol>
+        </div>
+
+        <div>
+          <h2 className="font-semibold">Alternative applications</h2>
+
+          <ol className="list-disc list-inside">
+            <li>
+              Tsurukame for iOS works nice, but having to press-hold to reveal answers in its Anki mode makes it
+              unbearably slow.
+            </li>
+            <li>
+              The WaniKani website/app is great, especially if you spice it up with userscripts, but it doesn't support
+              single-card mode.
+            </li>
+            <li>I'm generally not a fan of mobile apps, so this was the easiest solution for me.</li>
           </ol>
         </div>
 
